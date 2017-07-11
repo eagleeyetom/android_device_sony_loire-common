60b68d8b40c82692d17e623ad6ccb65cae5a3bb1
eagleeyetom <tomek.kondrat@gmail.com>
2017-07-11 14:56:48 +0200

    suzu: add encryption flags
    Change-Id: Icce3764749993f6836dcfd4cb8045d92e3de10c5

--- twrp.mk
+++ twrp.mk
@@ -32,3 +32,7 @@ TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_EXCLUDE_SUPERSU := true
 TWHAVE_SELINUX := true
+TARGET_HW_DISK_ENCRYPTION := true
+TARGET_RECOVERY_DEVICE_MODULES := libbinder libgui libui libEGL libGLES_trace libGLESv2 libprotobuf-cpp-lite libsync                       
+TW_RECOVERY_ADDITIONAL_RELINK_FILES := $(OUT)/system/lib64/libbinder.so $(OUT)/system/lib64/libgui.so $(OUT)/system/lib64/libui.so $(OUT)/system/lib64/libEGL.so $(OUT)/system/lib64/libGLES_trace.so $(OUT)/system/lib64/libGLESv2.so $(OUT)/system/lib64/libprotobuf-cpp-lite.so $(OUT)/system/lib64/libsync.so
+
